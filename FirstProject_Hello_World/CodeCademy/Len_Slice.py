5942959cbae7a4403bb61e145a4b55714f4826fc
Martin082 <146066671+Martin082@users.noreply.github.com>
2024-09-08 21:11:51 +0200

    Add files via upload
    You work at Len’s Slice, a new pizza joint in the neighborhood. You are going to use your knowledge of Python lists to organize some of your sales data.

---
+++ FirstProject_Hello_World/CodeCademy/Len_Slice.py
@@ -0,0 +1,28 @@
+toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
+prices = [2, 6, 1, 3, 2, 7, 2]
+
+num_two_dollar_slices = prices.count(2)
+print(num_two_dollar_slices)
+
+num_pizzas = len(toppings)
+print(num_pizzas)
+
+print(f"We sell {num_pizzas} different kinds of pizza!")
+
+pizza_and_prices = [
+  [2, 'pepperoni'], [6, 'pineapple'], [1, 'cheese'], [3, 'sausage'], [2, 'olives'], [7, 'anchovies'], [2, 'mushrooms']
+  ]
+
+pizza_and_prices.append([2.5, "peppers"])
+pizza_and_prices.sort()
+print(pizza_and_prices)
+
+cheapest_pizza = pizza_and_prices[1]
+
+most_expensive_pizza = pizza_and_prices[-1]
+pizza_and_prices.pop()
+
+
+three_cheapest = pizza_and_prices[:3]
+print(three_cheapest)
+
